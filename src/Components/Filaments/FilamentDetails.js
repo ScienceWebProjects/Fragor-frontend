51046800c39fb8a4c481b9abcd9409677cbf0f35
Ksar2911 <p.goraj0@gmail.com>
2023-11-14 18:03:49 +0100

    DPA-80 /front/ add renew filament btn

--- src/Components/Filaments/FilamentDetails.js
+++ src/Components/Filaments/FilamentDetails.js
@@ -2,6 +2,8 @@
 
 // hooks
 import { useState, useEffect } from 'react';
+import useToken from '../../Hooks/useToken';
+import { useNavigate } from 'react-router-dom';
 
 // components
 import TopBar from '../_shared/TopBar';
@@ -18,17 +20,93 @@ import '../_shared/UI/_details-buttons.scss';
 import './scss/_details-filament.scss';
 
 function FilamentDetails(props) {
+  const user = useToken();
+  const navigate = useNavigate();
+
   const [details, setDetails] = useState(props.details);
+
+  // variables for boxes
   const [editBox, setEditBox] = useState(false);
   const [deleteBox, setDeleteBox] = useState(false);
   const [notesBox, setNotesBox] = useState(false);
+
+  // variables for error message
+  const [isError, setIsError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [errorCallback, setErrorCallback] = useState(() => {});
 
   useEffect(() => {
     setTimeout(() => {
       const storedFilament = sessionStorage.getItem('filamentDetails');
       setDetails(JSON.parse(storedFilament));
     }, 50);
-  }, []);
+  });
+
+  const renewDelete = async () => {
+    const requestOptions = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${user.token}`,
+      },
+    };
+
+    try {
+      const response = await fetch(
+        `${props.api.ip}${props.api.filamentDelete_id}${props.details.id}/`,
+        requestOptions
+      );
+
+      if (response.status === 204) {
+        alert('Succesfull filament deleted for renew.');
+      }
+
+      if (response.status === 404) {
+        alert(response.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const renewHandler = async () => {
+    const addData = {
+      material: details.material,
+      color: details.color,
+      brand: details.brand,
+      diameter: details.diameter,
+      price: details.price,
+      uid: details.uid,
+    };
+
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${user.token}`,
+      },
+      body: JSON.stringify(addData),
+    };
+
+    try {
+      const response = await fetch(
+        `${props.api.ip}${props.api.filamentAdd}`,
+        requestOptions
+      );
+
+      if (response.status === 201) {
+        renewDelete();
+        alert('Successfull renew filament.');
+        navigate(props.api.filamentsPage);
+      }
+      if (response.status === 400 || response.status === 404) {
+        alert(response.message);
+      }
+    } catch (error) {
+      console.log(error);
+      alert('Somethint went bad.');
+    }
+  };
 
   if (!details) {
     return <div>No filament selected.</div>;
@@ -50,6 +128,13 @@ function FilamentDetails(props) {
             }}
           >
             Edit
+          </Button>
+          <Button
+            className='wrapper-btn'
+            color='blue'
+            onClick={() => renewHandler()}
+          >
+            Renew
           </Button>
           <Button
             className='wrapper-btn'
@@ -78,7 +163,8 @@ function FilamentDetails(props) {
             <div>Material: {details.material}</div>
             <div>Color: {details.color}</div>
             <div>Brand: {details.brand}</div>
-            <div>Diameter: {details.diameter}</div>
+            <div>Diameter: {details.diameter || 'No data.'}</div>
+            <div>Price: {details.price || 'No data.'}</div>
           </section>
         </div>
       </main>
