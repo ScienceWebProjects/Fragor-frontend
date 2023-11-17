e88dba6da169478ed45a409a55ce31c6625f453b
Ksar2911 <p.goraj0@gmail.com>
2023-11-17 14:43:14 +0100

    first steps for AI

--- src/Components/Charts/Charts.js
+++ src/Components/Charts/Charts.js
@@ -1,6 +1,7 @@
 // libs
 
 // hooks
+import { useState, useEffect } from 'react';
 import useToken from '../../Hooks/useToken';
 import usePermissions from '../../Hooks/usePermissions';
 
@@ -17,6 +18,32 @@ import Button from '../UI/shared/buttons/Button';
 function Charts({ api }) {
   const user = useToken();
   const permission = usePermissions(user);
+
+  const chartsDataApiCall = async () => {
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${user.token}`,
+      },
+    };
+
+    try {
+      const response = await fetch(
+        `${api.AI_ip}${api.AIChartsDataGet}`,
+        requestOptions
+      );
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    chartsDataApiCall();
+  });
 
   if (permission.logged === 'logout') {
     return <LogoutUser api={api} />;
